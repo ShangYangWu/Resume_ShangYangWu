0dddc038d3ba736d2402f4fac98640cc156ec5a8
ShangYangWu <66379608+ShangYangWu@users.noreply.github.com>
2020-09-27 21:41:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    document.getElementById("navbar").style = "background-color:rgb(56, 140, 232,0.8)";
+    document.getElementById("navbar").style = "background-color:rgb(56, 140, 232,1)";
   } else {
     document.getElementById("navbar").style = "background-color:rgb(56, 140, 232,0)";
   }
